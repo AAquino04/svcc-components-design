87a28a3c793cf746b287df8f0525a277e28cbc38
Anderson Aquino <johndoe@example.com>
2022-03-14 12:55:17 -0300

    refactor card session into 2 components: Session and Sessions

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "adf4da9541833d30"; }
+/******/ 		__webpack_require__.h = function() { return "fffab6c1a0709e85"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
