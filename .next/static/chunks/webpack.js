d16d73d16752193d0cbee2ce7b2a7fa68cf724c7
Anderson Aquino <johndoe@example.com>
2022-03-17 12:32:11 -0300

    add commit before stash pop

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9038fbda3828a383"; }
+/******/ 		__webpack_require__.h = function() { return "1d26a0a05182097a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
